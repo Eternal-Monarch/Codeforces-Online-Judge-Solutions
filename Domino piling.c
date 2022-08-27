6dffa5babede81f0738d6482374873b8fc9de9fa
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2022-08-27 12:45:18 +0530

    Create Domino piling.c

---
+++ Domino piling.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    int a;
+    int b;
+    int count=0;
+    int temp=0;
+    int array[100];
+    scanf("%d %d",&a,&b);
+    printf("%d",(a*b)/2);
+    return 0;
+}
